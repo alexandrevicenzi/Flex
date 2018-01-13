928e4c1067e16b18e84e116c231005b2f6b0faa3
Alexandre Vicenzi <vicenzi.alexandre@gmail.com>
2018-01-13 01:43:11 -0200

    Blog.

--- docs/publishconf.py
+++ docs/publishconf.py
@@ -7,8 +7,7 @@ sys.path.append(os.curdir)
 
 from pelicanconf import *
 
-SITEURL = 'https://blog.alexandrevicenzi.com'
+SITEURL = 'http://flex.alxd.me/blog'
 RELATIVE_URLS = False
 USE_LESS = False
-DELETE_OUTPUT_DIRECTORY = True
-GOOGLE_ANALYTICS = "UA-55543164-3"
+GOOGLE_ANALYTICS = "UA-55543164-6"
