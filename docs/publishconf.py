8e4a88edde063901715733e7662fdcd7d0ab079f
Alexandre Vicenzi <vicenzi.alexandre@gmail.com>
2020-04-20 23:38:29 +0200

    Update docs

--- docs/publishconf.py
+++ docs/publishconf.py
@@ -6,6 +6,22 @@ sys.path.append(os.curdir)
 from pelicanconf import *
 
 SITEURL = 'http://flex.alxd.me/blog'
+
 RELATIVE_URLS = False
+
 USE_LESS = False
+
 GOOGLE_ANALYTICS = "UA-55543164-6"
+
+GOOGLE_ADSENSE = {
+    'ca_id': 'ca-pub-6625957038449899',
+    'page_level_ads': True,
+    'ads': {
+        'aside': '8752710348',
+        'main_menu': '',
+        'index_top': '',
+        'index_bottom': '1124188687',
+        'article_top': '',
+        'article_bottom': '4843941849',
+    }
+}
