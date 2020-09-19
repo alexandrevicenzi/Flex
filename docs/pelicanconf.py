ca51e4c5442884c4ec3e3463dd74724f120dae04
Alexandre Vicenzi <vicenzi.alexandre@gmail.com>
2020-09-19 22:04:09 +0200

    Black format

--- docs/pelicanconf.py
+++ docs/pelicanconf.py
@@ -1,22 +1,22 @@
 from datetime import datetime
 
-AUTHOR = 'Alexandre Vicenzi'
-SITEURL = 'http://localhost:8000'
-SITENAME = 'Flex'
-SITETITLE = 'Flex'
-SITESUBTITLE = 'The minimalist Pelican theme'
-SITEDESCRIPTION = 'Flex - The minimalist Pelican theme.'
+AUTHOR = "Alexandre Vicenzi"
+SITEURL = "http://localhost:8000"
+SITENAME = "Flex"
+SITETITLE = "Flex"
+SITESUBTITLE = "The minimalist Pelican theme"
+SITEDESCRIPTION = "Flex - The minimalist Pelican theme."
 # SITELOGO = ''
 # FAVICON = '/images/favicon.ico'
-BROWSER_COLOR = '#333333'
-PYGMENTS_STYLE = 'monokai'
+BROWSER_COLOR = "#333333"
+PYGMENTS_STYLE = "monokai"
 
-ROBOTS = 'index, follow'
+ROBOTS = "index, follow"
 
-THEME = '../'
-PATH = 'content'
-OUTPUT_PATH = 'blog/'
-TIMEZONE = 'America/New_York'
+THEME = "../"
+PATH = "content"
+OUTPUT_PATH = "blog/"
+TIMEZONE = "America/New_York"
 
 # PLUGIN_PATHS = ['pelican-plugins']
 
@@ -24,17 +24,17 @@ TIMEZONE = 'America/New_York'
 
 # JINJA_ENVIRONMENT = {'extensions': ['jinja2.ext.i18n']}
 
-I18N_TEMPLATES_LANG = 'en'
-DEFAULT_LANG = 'en'
-OG_LOCALE = 'en_US'
-LOCALE = 'en_US'
+I18N_TEMPLATES_LANG = "en"
+DEFAULT_LANG = "en"
+OG_LOCALE = "en_US"
+LOCALE = "en_US"
 
 DATE_FORMATS = {
-    'en': '%B %d, %Y',
+    "en": "%B %d, %Y",
 }
 
-FEED_ALL_ATOM = 'feeds/all.atom.xml'
-CATEGORY_FEED_ATOM = 'feeds/{slug}.atom.xml'
+FEED_ALL_ATOM = "feeds/all.atom.xml"
+CATEGORY_FEED_ATOM = "feeds/{slug}.atom.xml"
 TRANSLATION_FEED_ATOM = None
 AUTHOR_FEED_ATOM = None
 AUTHOR_FEED_RSS = None
@@ -44,31 +44,33 @@ MAIN_MENU = True
 HOME_HIDE_TAGS = True
 
 SOCIAL = (
-    ('github', 'https://github.com/alexandrevicenzi/Flex'),
-    ('rss', '/blog/feeds/all.atom.xml'),
+    ("github", "https://github.com/alexandrevicenzi/Flex"),
+    ("rss", "/blog/feeds/all.atom.xml"),
 )
 
-MENUITEMS = (('Archives', '/archives.html'),
-             ('Categories', '/categories.html'),
-             ('Tags', '/tags.html'),)
+MENUITEMS = (
+    ("Archives", "/archives.html"),
+    ("Categories", "/categories.html"),
+    ("Tags", "/tags.html"),
+)
 
 CC_LICENSE = {
-    'name': 'Creative Commons Attribution-ShareAlike',
-    'version': '4.0',
-    'slug': 'by-sa'
+    "name": "Creative Commons Attribution-ShareAlike",
+    "version": "4.0",
+    "slug": "by-sa",
 }
 
 COPYRIGHT_YEAR = datetime.now().year
 DEFAULT_PAGINATION = 10
 
 DISQUS_SITENAME = "flex-pelican"
-ADD_THIS_ID = 'ra-55adbb025d4f7e55'
+ADD_THIS_ID = "ra-55adbb025d4f7e55"
 
-STATIC_PATHS = ['images', 'extra/ads.txt', 'extra/CNAME']
+STATIC_PATHS = ["images", "extra/ads.txt", "extra/CNAME"]
 
 EXTRA_PATH_METADATA = {
-    'extra/ads.txt': {'path': 'ads.txt'},
-    'extra/CNAME': {'path': 'CNAME'},
+    "extra/ads.txt": {"path": "ads.txt"},
+    "extra/CNAME": {"path": "CNAME"},
 }
 
 THEME_COLOR_AUTO_DETECT_BROWSER_PREFERENCE = True
--- docs/publishconf.py
+++ docs/publishconf.py
@@ -9,7 +9,7 @@ except ImportError:
     sys.path.append(os.path.join(os.curdir, "docs"))
     from pelicanconf import *
 
-SITEURL = 'https://flex.alxd.me/'
+SITEURL = "https://flex.alxd.me/"
 
 RELATIVE_URLS = False
 
@@ -18,14 +18,14 @@ USE_LESS = False
 GOOGLE_ANALYTICS = "UA-55543164-6"
 
 GOOGLE_ADSENSE = {
-    'ca_id': 'ca-pub-6625957038449899',
-    'page_level_ads': True,
-    'ads': {
-        'aside': '8752710348',
-        'main_menu': '',
-        'index_top': '',
-        'index_bottom': '1124188687',
-        'article_top': '',
-        'article_bottom': '4843941849',
-    }
+    "ca_id": "ca-pub-6625957038449899",
+    "page_level_ads": True,
+    "ads": {
+        "aside": "8752710348",
+        "main_menu": "",
+        "index_top": "",
+        "index_bottom": "1124188687",
+        "article_top": "",
+        "article_bottom": "4843941849",
+    },
 }
